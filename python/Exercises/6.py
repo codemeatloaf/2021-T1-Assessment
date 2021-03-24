65ac1f83b0ad4ac75734875f15169c9bb8f649a6
codemeatloaf <65986665+codemeatloaf@users.noreply.github.com>
2021-03-24 14:50:56 +1000

    commit on main

--- python/Exercises/6.py
+++ python/Exercises/6.py
@@ -3,3 +3,17 @@
 list1 = input("Input the elements of the list seperated by spaces: ")
 list2 = list1.split()
 
+def isPalindrome(s):
+    return s == s[::-1]
+
+# to test that it works splits it into a list
+# print("List: ", list2) 
+
+s = list2
+
+ans = isPalindrome(s)
+
+if ans:
+    print("Yes")
+else:
+    print("No")
