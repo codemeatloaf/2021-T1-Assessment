cd5c7765e291babe703824940266ad1796b815e8
codemeatloaf <65986665+codemeatloaf@users.noreply.github.com>
2021-03-24 14:38:03 +1000

    commit on main

--- python/Exercises/6.py
+++ python/Exercises/6.py
@@ -1,3 +1,5 @@
 # Ask the user for a string and print out whether this string is a palindrome or not
 
-list1 = int(input("Input the elements of the list seperated by spaces: "))
+list1 = input("Input the elements of the list seperated by spaces: ")
+list2 = list1.split()
+
